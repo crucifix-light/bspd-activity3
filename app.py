aa3bc2507562b442a8f316d85346e4d74668f95a
unknown <crucifix.sacrifice.light@gmail.com>
2025-02-11 07:57:12 +0800

    Updated commit

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to flask home JR!"
+    return "Welcome to flask home!"
 
 @app.route('/page1')
 def page1():
-    return "Welcome to flask page1 JR!"
+    return "Welcome to flask page1!"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8087)
